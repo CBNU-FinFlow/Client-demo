61e3372f656b137e55c7eebf88f09d1290f1ed87
reo91004 <reo91004@gmail.com>
2024-12-18 22:18:23 +0900

    feat: 로그인/회원가입 백엔드 기능 추가, 모달 추가

--- src/app/(afterLogin)/components/Navbar.tsx
+++ src/app/(afterLogin)/components/Navbar.tsx
@@ -6,157 +6,151 @@ import Link from "next/link";
 import { usePathname } from "next/navigation";
 import { useState } from "react";
 import { useCurrencyStore } from "@/app/store/currency.ts";
+import { useRouter } from "next/navigation"; // useRouter 추가
+import Modal from "@/app/(afterLogin)/components/Modal"; // Modal 컴포넌트 가져오기
 
 export default function Navbar() {
-    const pathname = usePathname();
-    const { selectedCurrency, currencies, setCurrency } = useCurrencyStore();
-    const [currencyOpen, setCurrencyOpen] = useState(false);
-    const [profileOpen, setProfileOpen] = useState(false);
+	const pathname = usePathname();
+	const { selectedCurrency, currencies, setCurrency } = useCurrencyStore();
+	const [currencyOpen, setCurrencyOpen] = useState(false);
+	const [profileOpen, setProfileOpen] = useState(false);
+	const [logoutModalOpen, setLogoutModalOpen] = useState(false); // 로그아웃 모달 상태
+	const router = useRouter(); // 페이지 이동을 위한 useRouter
 
-    const getLinkClassName = (path: string) => {
-        return pathname === path
-            ? "px-3 py-[0.4rem] rounded-[0.4rem] bg-slate-100 text-[#3699ff]"
-            : "px-3 py-[0.4rem] rounded-[0.4rem] text-slate-400 hover:bg-slate-100 hover:text-[#3699ff] transition-all";
-    };
+	const getLinkClassName = (path: string) => {
+		return pathname === path
+			? "px-3 py-[0.4rem] rounded-[0.4rem] bg-slate-100 text-[#3699ff]"
+			: "px-3 py-[0.4rem] rounded-[0.4rem] text-slate-400 hover:bg-slate-100 hover:text-[#3699ff] transition-all";
+	};
 
-    const toggleCurrencyDropdown = () => {
-        setCurrencyOpen(!currencyOpen);
-        setProfileOpen(false);
-    };
+	const toggleCurrencyDropdown = () => {
+		setCurrencyOpen(!currencyOpen);
+		setProfileOpen(false);
+	};
 
-    const toggleProfileDropdown = () => {
-        setProfileOpen(!profileOpen);
-        setCurrencyOpen(false);
-    };
+	const toggleProfileDropdown = () => {
+		setProfileOpen(!profileOpen);
+		setCurrencyOpen(false);
+	};
 
-    return (
-        <nav className="w-full h-[4.25rem] fixed bg-white flex justify-between items-center drop-shadow-xl z-50">
-            <div className="mx-40 flex gap-[2rem] items-center">
-                <Link href="/">
-                    <Image src={Logo} alt="logo" width={180} />
-                </Link>
-                <Link href="/" className={getLinkClassName("/")}>
-                    홈
-                </Link>
-                <Link href="/news" className={getLinkClassName("/news")}>
-                    뉴스
-                </Link>
-                <Link href="/stats" className={getLinkClassName("/stats")}>
-                    분석
-                </Link>
-                <Link
-                    href="/portfolio"
-                    className={getLinkClassName("/portfolio")}
-                >
-                    포트폴리오
-                </Link>
-                <Link
-                    href="/community"
-                    className={getLinkClassName("/community")}
-                >
-                    커뮤니티
-                </Link>
-            </div>
-            <div className="mx-40 flex gap-[1.5rem] items-center">
-                {/* Currency Dropdown */}
-                <div className="relative">
-                    <button
-                        onClick={toggleCurrencyDropdown}
-                        className="flex items-center px-3 py-[0.4rem] rounded-[0.4rem] text-slate-400 hover:bg-slate-100 hover:text-[#3699ff] transition-all"
-                    >
-                        {selectedCurrency}
-                        <svg
-                            className={`-mr-1 size-5 text-gray-400 transition-transform ${
-                                currencyOpen ? "rotate-180" : ""
-                            }`}
-                            viewBox="0 0 20 20"
-                            fill="currentColor"
-                            aria-hidden="true"
-                        >
-                            <path
-                                fillRule="evenodd"
-                                d="M5.22 8.22a.75.75 0 0 1 1.06 0L10 11.94l3.72-3.72a.75.75 0 1 1 1.06 1.06l-4.25 4.25a.75.75 0 0 1-1.06 0L5.22 9.28a.75.75 0 0 1 0-1.06Z"
-                                clipRule="evenodd"
-                            />
-                        </svg>
-                    </button>
-                    {currencyOpen && (
-                        <div
-                            className="absolute right-0 mt-1 w-32 rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 overflow-y-auto"
-                            style={{ maxHeight: "200px" }} // 스크롤 높이 제한
-                        >
-                            <div className="py-1" role="menu">
-                                {currencies.map((currency) => (
-                                    <button
-                                        key={currency}
-                                        onClick={() => {
-                                            setCurrency(currency);
-                                            setCurrencyOpen(false);
-                                        }}
-                                        className="block w-full px-4 py-2 text-sm text-slate-400 hover:bg-slate-100 hover:text-[#3699ff] text-left"
-                                        role="menuitem"
-                                    >
-                                        {currency}
-                                    </button>
-                                ))}
-                            </div>
-                        </div>
-                    )}
-                </div>
+	const handleLogout = () => {
+		localStorage.clear(); // localStorage 삭제
+		setLogoutModalOpen(true); // 로그아웃 성공 모달 열기
+	};
 
-                {/* Profile Dropdown */}
-                <div className="relative">
-                    <button
-                        onClick={toggleProfileDropdown}
-                        className="p-1 rounded-[0.4rem] text-slate-400 hover:bg-slate-100 hover:text-[#3699ff] transition-all"
-                    >
-                        <svg
-                            width="24px"
-                            height="24px"
-                            viewBox="0 0 24 24"
-                            xmlns="http://www.w3.org/2000/svg"
-                        >
-                            <g
-                                stroke="none"
-                                strokeWidth="1"
-                                fill="none"
-                                fillRule="evenodd"
-                            >
-                                <path
-                                    d="M12,11 C9.79,11 8,9.21 8,7 C8,4.79 9.79,3 12,3 C14.21,3 16,4.79 16,7 C16,9.21 14.21,11 12,11 Z"
-                                    fill="currentColor"
-                                    opacity="0.3"
-                                ></path>
-                                <path
-                                    d="M3,20.2 C3.39,15.43 7.26,13 11.98,13 C16.77,13 20.7,15.29 21,20.2 C21.01,20.4 21,21 20.25,21 C16.54,21 11.03,21 3.72,21 C3.48,21 2.98,20.46 3,20.2 Z"
-                                    fill="currentColor"
-                                ></path>
-                            </g>
-                        </svg>
-                    </button>
-                    {profileOpen && (
-                        <div className="absolute right-0 mt-1 w-52 rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5">
-                            <div className="py-1" role="menu">
-                                {[
-                                    "프로필 정보",
-                                    "내 계정",
-                                    "알림",
-                                    "FAQ",
-                                    "로그아웃",
-                                ].map((item) => (
-                                    <button
-                                        key={item}
-                                        className="block w-full px-4 py-2 text-sm text-slate-400 hover:bg-slate-100 hover:text-[#3699ff] text-left"
-                                        role="menuitem"
-                                    >
-                                        {item}
-                                    </button>
-                                ))}
-                            </div>
-                        </div>
-                    )}
-                </div>
-            </div>
-        </nav>
-    );
+	return (
+		<nav className="w-full h-[4.25rem] fixed bg-white flex justify-between items-center drop-shadow-xl z-50">
+			<div className="mx-40 flex gap-[2rem] items-center">
+				<Link href="/">
+					<Image src={Logo} alt="logo" width={180} />
+				</Link>
+				<Link href="/" className={getLinkClassName("/")}>
+					홈
+				</Link>
+				<Link href="/news" className={getLinkClassName("/news")}>
+					뉴스
+				</Link>
+				<Link href="/stats" className={getLinkClassName("/stats")}>
+					분석
+				</Link>
+				<Link href="/portfolio" className={getLinkClassName("/portfolio")}>
+					포트폴리오
+				</Link>
+				<Link href="/community" className={getLinkClassName("/community")}>
+					커뮤니티
+				</Link>
+			</div>
+			<div className="mx-40 flex gap-[1.5rem] items-center">
+				{/* Currency Dropdown */}
+				<div className="relative">
+					<button onClick={toggleCurrencyDropdown} className="flex items-center px-3 py-[0.4rem] rounded-[0.4rem] text-slate-400 hover:bg-slate-100 hover:text-[#3699ff] transition-all">
+						{selectedCurrency}
+						<svg className={`-mr-1 size-5 text-gray-400 transition-transform ${currencyOpen ? "rotate-180" : ""}`} viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
+							<path fillRule="evenodd" d="M5.22 8.22a.75.75 0 0 1 1.06 0L10 11.94l3.72-3.72a.75.75 0 1 1 1.06 1.06l-4.25 4.25a.75.75 0 0 1-1.06 0L5.22 9.28a.75.75 0 0 1 0-1.06Z" clipRule="evenodd" />
+						</svg>
+					</button>
+					{currencyOpen && (
+						<div
+							className="absolute right-0 mt-1 w-32 rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 overflow-y-auto"
+							style={{ maxHeight: "200px" }} // 스크롤 높이 제한
+						>
+							<div className="py-1" role="menu">
+								{currencies.map((currency) => (
+									<button
+										key={currency}
+										onClick={() => {
+											setCurrency(currency);
+											setCurrencyOpen(false);
+										}}
+										className="block w-full px-4 py-2 text-sm text-slate-400 hover:bg-slate-100 hover:text-[#3699ff] text-left"
+										role="menuitem"
+									>
+										{currency}
+									</button>
+								))}
+							</div>
+						</div>
+					)}
+				</div>
+
+				{/* Profile Dropdown */}
+				<div className="relative">
+					<button onClick={toggleProfileDropdown} className="p-1 rounded-[0.4rem] text-slate-400 hover:bg-slate-100 hover:text-[#3699ff] transition-all">
+						<svg width="24px" height="24px" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+							<g stroke="none" strokeWidth="1" fill="none" fillRule="evenodd">
+								<path d="M12,11 C9.79,11 8,9.21 8,7 C8,4.79 9.79,3 12,3 C14.21,3 16,4.79 16,7 C16,9.21 14.21,11 12,11 Z" fill="currentColor" opacity="0.3"></path>
+								<path
+									d="M3,20.2 C3.39,15.43 7.26,13 11.98,13 C16.77,13 20.7,15.29 21,20.2 C21.01,20.4 21,21 20.25,21 C16.54,21 11.03,21 3.72,21 C3.48,21 2.98,20.46 3,20.2 Z"
+									fill="currentColor"
+								></path>
+							</g>
+						</svg>
+					</button>
+					{profileOpen && (
+						<div className="absolute right-0 mt-1 w-52 rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5">
+							<div className="py-1" role="menu">
+								{["프로필 정보", "내 계정", "알림", "FAQ", "로그아웃"].map((item) => (
+									<button
+										key={item}
+										onClick={() => {
+											if (item === "로그아웃") {
+												handleLogout();
+											}
+										}}
+										className="block w-full px-4 py-2 text-sm text-slate-400 hover:bg-slate-100 hover:text-[#3699ff] text-left"
+										role="menuitem"
+									>
+										{item}
+									</button>
+								))}
+							</div>
+						</div>
+					)}
+				</div>
+			</div>
+			{/* 로그아웃 성공 모달 */}
+			{logoutModalOpen && (
+				<Modal>
+					<div className="bg-white p-6 rounded-[1rem] shadow-lg w-[400px] text-center animate-fade-up">
+						<div className="flex justify-center items-center mb-4">
+							<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="48" height="48" className="text-green-500 animate-bounce">
+								<path fill="currentColor" d="M9 19a1 1 0 0 1-.707-.293l-5-5a1 1 0 0 1 1.414-1.414L9 16.586l10.293-10.293a1 1 0 0 1 1.414 1.414l-11 11A1 1 0 0 1 9 19Z" />
+							</svg>
+						</div>
+						<h2 className="text-xl font-semibold text-slate-700 mb-4">로그아웃 성공</h2>
+						<p className="text-slate-500 mb-6">홈페이지로 이동합니다.</p>
+						<button
+							onClick={() => {
+								setLogoutModalOpen(false);
+								router.push("/");
+							}}
+							className="w-full bg-[#3699ff] hover:bg-[#1086ff] text-white px-4 py-2 rounded-lg transition-colors shadow-md"
+						>
+							확인
+						</button>
+					</div>
+				</Modal>
+			)}
+		</nav>
+	);
 }
--- src/app/(beforeLogin)/auth/login/page.tsx
+++ src/app/(beforeLogin)/auth/login/page.tsx
@@ -4,88 +4,138 @@ import Image from "next/image";
 import Logo from "../../../../../public/images/logo.png";
 import Link from "next/link";
 import { useState } from "react";
+import { useRouter } from "next/navigation"; // 페이지 이동을 위한 useRouter 추가
+import axios from "axios";
+import { signInWithEmailAndPassword } from "@/firebase"; // Firebase 로그인 함수 추가
+import { auth } from "@/firebase"; // Firebase 인증 모듈 추가
+import Modal from "@/app/(afterLogin)/components/Modal"; // Modal 컴포넌트 가져오기
 
 export default function Page() {
-    const [showPassword, setShowPassword] = useState(false);
+	const router = useRouter();
+	const [showPassword, setShowPassword] = useState(false);
+	const [email, setEmail] = useState("");
+	const [password, setPassword] = useState("");
+	const [successModalOpen, setSuccessModalOpen] = useState(false);
+	const [failureModalOpen, setFailureModalOpen] = useState(false);
+	const [errorMessage, setErrorMessage] = useState("");
 
-    const togglePasswordVisibility = () => {
-        setShowPassword((prev) => !prev);
-    };
+	const togglePasswordVisibility = () => setShowPassword((prev) => !prev);
 
-    return (
-        <div className="w-[22rem] mx-auto flex flex-col items-center justify-center min-h-screen">
-            <Image
-                src={Logo}
-                alt="logo"
-                width={400}
-                className="mb-[4.5rem]"
-            />
-            <form className="slide-up w-full">
-                <h1 className="text-[1.7rem] font-bold mb-9 text-slate-700">
-                    로그인
-                </h1>
-                <input
-                    type="text"
-                    placeholder="이메일"
-                    className="w-full h-14 p-6 bg-slate-100 rounded-[0.75rem] mb-6 outline-none"
-                ></input>
-                <div className="w-full flex relative mb-6">
-                    <input
-                        type={showPassword ? "text" : "password"}
-                        placeholder="비밀번호"
-                        className="w-full h-14 p-6 bg-slate-100 rounded-[0.75rem] outline-none"
-                    />
-                    <button
-                        type="button"
-                        onClick={togglePasswordVisibility}
-                        className="flex items-center justify-center absolute w-12 h-full right-0 bg-[#e1f0ff] rounded-tr-[0.75rem] rounded-br-[0.75rem]"
-                    >
-                        {showPassword ? (
-                            <svg
-                                xmlns="http://www.w3.org/2000/svg"
-                                height="18"
-                                viewBox="0 -960 960 960"
-                                width="18"
-                                fill="#3699ff"
-                            >
-                                <path d="M630.922-441.078 586-486q9-49.693-28.346-89.346Q520.307-615 466-606l-44.922-44.922q13.538-6.077 27.769-9.115 14.23-3.039 31.153-3.039 68.076 0 115.576 47.5T643.076-500q0 16.923-3.039 31.538-3.038 14.615-9.115 27.384Zm127.231 124.462L714-358q38-29 67.5-63.5T832-500q-50-101-143.5-160.5T480-720q-29 0-57 4t-55 12l-46.614-46.614q37.923-15.077 77.461-22.231 39.538-7.154 81.153-7.154 140.615 0 253.614 77.538 113 77.539 164.846 202.461-22.231 53.615-57.423 100.076-35.192 46.461-82.884 83.308Zm32.308 231.383L628.616-245.848q-30.769 11.385-68.192 18.616Q523-220.001 480-220.001q-140.999 0-253.614-77.538Q113.771-375.078 61.54-500q22.154-53 57.231-98.885 35.077-45.884 77.231-79.576l-110.77-112 42.154-42.153 705.228 705.228-42.153 42.153ZM238.155-636.309q-31.692 25.231-61.654 60.655Q146.539-540.231 128-500q50 101 143.5 160.5T480-280q27.308 0 54.386-4.616 27.077-4.615 45.923-9.538l-50.616-51.847q-10.231 4.153-23.693 6.615-13.461 2.462-26 2.462-68.076 0-115.576-47.5T316.924-500q0-12.154 2.462-25.423 2.462-13.27 6.615-24.27l-87.846-86.616ZM541-531Zm-131.768 65.769Z"></path>
-                            </svg>
-                        ) : (
-                            <svg
-                                xmlns="http://www.w3.org/2000/svg"
-                                height="18"
-                                viewBox="0 -960 960 960"
-                                width="18"
-                                fill="#3699ff"
-                            >
-                                <path d="M480.091-336.924q67.985 0 115.485-47.59 47.5-47.591 47.5-115.577 0-67.985-47.59-115.485-47.591-47.5-115.577-47.5-67.985 0-115.485 47.59-47.5 47.591-47.5 115.577 0 67.985 47.59 115.485 47.591 47.5 115.577 47.5ZM480-392q-45 0-76.5-31.5T372-500q0-45 31.5-76.5T480-608q45 0 76.5 31.5T588-500q0 45-31.5 76.5T480-392Zm.055 171.999q-137.977 0-251.439-76.115Q115.155-372.231 61.54-500q53.615-127.769 167.022-203.884 113.406-76.115 251.383-76.115t251.439 76.115Q844.845-627.769 898.46-500q-53.615 127.769-167.022 203.884-113.406 76.115-251.383 76.115ZM480-500Zm0 220q113 0 207.5-59.5T832-500q-50-101-144.5-160.5T480-720q-113 0-207.5 59.5T128-500q50 101 144.5 160.5T480-280Z"></path>
-                            </svg>
-                        )}
-                    </button>
-                </div>
-                <div className="w-full flex justify-between items-center mb-8">
-                    <Link
-                        href="#"
-                        className="text-slate-500 text-sm hover:text-[#3699ff]"
-                    >
-                        비밀번호 찾기
-                    </Link>
-                    <button className="bg-[#3699ff] hover:bg-[#1086ff] px-6 py-3 text-sm text-white rounded-[0.75rem]">
-                        로그인
-                    </button>
-                </div>
-                <div className="flex gap-2 items-center justify-content">
-                    <span className="text-slate-500 text-sm">
-                        아직 계정이 없으신가요?
-                    </span>
-                    <Link
-                        href="/auth/registration"
-                        className="text-[#3699ff] hover:text-[#1086ff] text-sm"
-                    >
-                        회원가입
-                    </Link>
-                </div>
-            </form>
-        </div>
-    );
+	const handleLogin = async (e: React.FormEvent) => {
+		e.preventDefault();
+		try {
+			const userCredential = await signInWithEmailAndPassword(auth, email, password);
+			const user = userCredential.user;
+
+			const formData = new URLSearchParams();
+			formData.append("username", email);
+			formData.append("password", password);
+
+			const response = await axios.post("http://localhost:8000/token", formData, {
+				headers: {
+					"Content-Type": "application/x-www-form-urlencoded",
+				},
+			});
+
+			const { access_token } = response.data;
+			localStorage.setItem("access_token", access_token);
+
+			setSuccessModalOpen(true); // 성공 모달 열기
+		} catch (err: any) {
+			console.error("로그인 실패:", err);
+			setErrorMessage("이메일 또는 비밀번호가 잘못되었습니다."); // 사용자 친화적인 메시지 설정
+			setFailureModalOpen(true); // 실패 모달 열기
+		}
+	};
+
+	return (
+		<div className="w-[22rem] mx-auto flex flex-col items-center justify-center min-h-screen">
+			<Image src={Logo} alt="logo" width={400} className="mb-[4.5rem]" />
+			<form onSubmit={handleLogin} className="slide-up w-full">
+				<h1 className="text-[1.7rem] font-bold mb-9 text-slate-700">로그인</h1>
+				<input type="text" placeholder="이메일" value={email} onChange={(e) => setEmail(e.target.value)} className="w-full h-14 p-6 bg-slate-100 rounded-[0.75rem] mb-6 outline-none"></input>
+				<div className="w-full flex relative mb-6">
+					<input
+						type={showPassword ? "text" : "password"}
+						placeholder="비밀번호"
+						value={password}
+						onChange={(e) => setPassword(e.target.value)}
+						className="w-full h-14 p-6 bg-slate-100 rounded-[0.75rem] outline-none"
+					/>
+					<button type="button" onClick={togglePasswordVisibility} className="flex items-center justify-center absolute w-12 h-full right-0 bg-[#e1f0ff] rounded-tr-[0.75rem] rounded-br-[0.75rem]">
+						{showPassword ? (
+							<svg xmlns="http://www.w3.org/2000/svg" height="18" viewBox="0 -960 960 960" width="18" fill="#3699ff">
+								<path d="M630.922-441.078 586-486q9-49.693-28.346-89.346Q520.307-615 466-606l-44.922-44.922q13.538-6.077 27.769-9.115 14.23-3.039 31.153-3.039 68.076 0 115.576 47.5T643.076-500q0 16.923-3.039 31.538-3.038 14.615-9.115 27.384Zm127.231 124.462L714-358q38-29 67.5-63.5T832-500q-50-101-143.5-160.5T480-720q-29 0-57 4t-55 12l-46.614-46.614q37.923-15.077 77.461-22.231 39.538-7.154 81.153-7.154 140.615 0 253.614 77.538 113 77.539 164.846 202.461-22.231 53.615-57.423 100.076-35.192 46.461-82.884 83.308Zm32.308 231.383L628.616-245.848q-30.769 11.385-68.192 18.616Q523-220.001 480-220.001q-140.999 0-253.614-77.538Q113.771-375.078 61.54-500q22.154-53 57.231-98.885 35.077-45.884 77.231-79.576l-110.77-112 42.154-42.153 705.228 705.228-42.153 42.153ZM238.155-636.309q-31.692 25.231-61.654 60.655Q146.539-540.231 128-500q50 101 143.5 160.5T480-280q27.308 0 54.386-4.616 27.077-4.615 45.923-9.538l-50.616-51.847q-10.231 4.153-23.693 6.615-13.461 2.462-26 2.462-68.076 0-115.576-47.5T316.924-500q0-12.154 2.462-25.423 2.462-13.27 6.615-24.27l-87.846-86.616ZM541-531Zm-131.768 65.769Z"></path>
+							</svg>
+						) : (
+							<svg xmlns="http://www.w3.org/2000/svg" height="18" viewBox="0 -960 960 960" width="18" fill="#3699ff">
+								<path d="M480.091-336.924q67.985 0 115.485-47.59 47.5-47.591 47.5-115.577 0-67.985-47.59-115.485-47.591-47.5-115.577-47.5-67.985 0-115.485 47.59-47.5 47.591-47.5 115.577 0 67.985 47.59 115.485 47.591 47.5 115.577 47.5ZM480-392q-45 0-76.5-31.5T372-500q0-45 31.5-76.5T480-608q45 0 76.5 31.5T588-500q0 45-31.5 76.5T480-392Zm.055 171.999q-137.977 0-251.439-76.115Q115.155-372.231 61.54-500q53.615-127.769 167.022-203.884 113.406-76.115 251.383-76.115t251.439 76.115Q844.845-627.769 898.46-500q-53.615 127.769-167.022 203.884-113.406 76.115-251.383 76.115ZM480-500Zm0 220q113 0 207.5-59.5T832-500q-50-101-144.5-160.5T480-720q-113 0-207.5 59.5T128-500q50 101 144.5 160.5T480-280Z"></path>
+							</svg>
+						)}
+					</button>
+				</div>
+				<div className="w-full flex justify-between items-center mb-8">
+					<Link href="#" className="text-slate-500 text-sm hover:text-[#3699ff]">
+						비밀번호 찾기
+					</Link>
+					<button type="submit" className="bg-[#3699ff] hover:bg-[#1086ff] px-6 py-3 text-sm text-white rounded-[0.75rem]">
+						로그인
+					</button>
+				</div>
+				<div className="flex gap-2 items-center justify-content">
+					<span className="text-slate-500 text-sm">아직 계정이 없으신가요?</span>
+					<Link href="/auth/registration" className="text-[#3699ff] hover:text-[#1086ff] text-sm">
+						회원가입
+					</Link>
+				</div>
+			</form>
+			{/* 성공 모달 */}
+			{successModalOpen && (
+				<Modal>
+					<div className="fixed inset-0 bg-black bg-opacity-30 flex justify-center items-center animate-fade-up">
+						<div className="bg-white p-6 rounded-[1rem] shadow-lg w-[400px] text-center">
+							<div className="flex justify-center items-center mb-4">
+								<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="48" height="48" className="text-green-500 animate-bounce">
+									<path fill="currentColor" d="M9 19a1 1 0 0 1-.707-.293l-5-5a1 1 0 0 1 1.414-1.414L9 16.586l10.293-10.293a1 1 0 0 1 1.414 1.414l-11 11A1 1 0 0 1 9 19Z" />
+								</svg>
+							</div>
+							<h2 className="text-xl font-semibold text-slate-700 mb-4">로그인 성공</h2>
+							<p className="text-slate-500 mb-6">포트폴리오 페이지로 이동합니다.</p>
+							<button
+								onClick={() => {
+									setSuccessModalOpen(false);
+									router.push("/portfolio");
+								}}
+								className="w-full bg-[#3699ff] hover:bg-[#1086ff] text-white px-4 py-2 rounded-lg transition-colors shadow-md"
+							>
+								확인
+							</button>
+						</div>
+					</div>
+				</Modal>
+			)}
+			{/* 실패 모달 */}
+			{failureModalOpen && (
+				<Modal>
+					<div className="fixed inset-0 bg-black bg-opacity-30 flex justify-center items-center animate-fade-up">
+						<div className="bg-white p-6 rounded-[1rem] shadow-lg w-[400px] text-center">
+							<div className="flex justify-center items-center mb-4">
+								<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="48" height="48" className="text-red-500 animate-bounce">
+									<path
+										fill="currentColor"
+										d="M6.343 6.343a1 1 0 0 1 1.414 0L12 10.586l4.243-4.243a1 1 0 1 1 1.414 1.414L13.414 12l4.243 4.243a1 1 0 0 1-1.414 1.414L12 13.414l-4.243 4.243a1 1 0 0 1-1.414-1.414L10.586 12 6.343 7.757a1 1 0 0 1 0-1.414Z"
+									/>
+								</svg>
+							</div>
+							<h2 className="text-xl font-semibold text-red-600 mb-4">로그인 실패</h2>
+							<p className="text-slate-500 mb-6">{errorMessage}</p>
+							<button onClick={() => setFailureModalOpen(false)} className="w-full bg-gray-400 hover:bg-gray-500 text-white px-4 py-2 rounded-lg transition-colors shadow-md">
+								닫기
+							</button>
+						</div>
+					</div>
+				</Modal>
+			)}
+		</div>
+	);
 }
